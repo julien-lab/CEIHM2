b62e2973e8843160a8b72ed00da1a865e094f45e
julienn.ndiaye59155@gmail.com <julienn.ndiaye59155@gmail.com>
2022-01-10 11:47:01 +0100

    add carousel example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  exports: [AccueilComponent],
+  bootstrap: [AccueilComponent]
 })
 export class AppModule { }
