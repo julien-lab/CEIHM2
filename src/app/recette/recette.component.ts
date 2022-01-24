6d6fcecc1f48535e8581b52cc5560636ebddfaf6
Benjamin-deLaverny <benjamin.de-laverny@etu.univ-cotedazur.fr>
2022-01-24 11:55:09 +0100

    Page de recette

--- src/app/recette/recette.component.ts
+++ src/app/recette/recette.component.ts
@@ -9,6 +9,9 @@ export class RecetteComponent implements OnInit {
 
   constructor() { }
 
+  public etapes = ['Mélangez le jus de gingembre avec la sauce de soja, et le sucre.', 'Ajoutez les morceaux de poulet à la sauce.', 'Couvrir et laissez reposer pendant 20 minutes.'];
+  public ingredients = ['Poulet', 'Riz', 'Sucre', 'Gingembre', 'Huile de sésame', 'Sauce soja'];
+  public notes = ['Très bon : 4', 'Excellent : 5', 'J\'ai bien aimé : 3'];
   ngOnInit(): void {
   }
 
