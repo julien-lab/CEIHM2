d72b2c621f81a16c3a96c9108c7b88e1c63014ba
Benjamin-deLaverny <benjamin.de-laverny@etu.univ-cotedazur.fr>
2022-01-31 11:49:53 +0100

    add to cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Route, Router} from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  message = false;
 
-  constructor() { }
+  constructor(private router: Router) {
+    if (router.getCurrentNavigation().extras.preserveFragment !== undefined) {
+      this.message = this.router.getCurrentNavigation().extras.preserveFragment.valueOf();
+    }
+  }
 
   ngOnInit(): void {
   }
--- src/app/recette/recette.component.ts
+++ src/app/recette/recette.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {NavigationExtras, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recette',
   templateUrl: './recette.component.html',
-  styleUrls: ['./recette.component.css']
+  styleUrls: ['./recette.component.css'],
 })
 export class RecetteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   public etapes = ['Mélangez le jus de gingembre avec la sauce de soja, et le sucre.', 'Ajoutez les morceaux de poulet à la sauce.', 'Couvrir et laissez reposer pendant 20 minutes.'];
   public ingredients = ['Poulet', 'Riz', 'Sucre', 'Gingembre', 'Huile de sésame', 'Sauce soja'];
   public notes = ['Très bon : 4', 'Excellent : 5', 'J\'ai bien aimé : 3'];
+
+
   ngOnInit(): void {
+  }
+
+  public addToCart(): void{
+    this.route.navigate(['/cart'], { preserveFragment: true });
   }
 
 }
