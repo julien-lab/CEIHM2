840f5ed31a991b887dd10ef8c4bede9db76d37c7
Benjamin-deLaverny <benjamin.de-laverny@etu.univ-cotedazur.fr>
2022-01-31 16:55:53 +0100

    cart css et html

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,15 +7,21 @@ import {Route, Router} from '@angular/router';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  message = false;
+  message = true;
+
+  public ingredients = ['2 blancs de poulet', '100g de riz', '2 càs de sucre', 'Gingembre', 'Huile de sésame', 'Sauce soja'];
 
   constructor(private router: Router) {
-    if (router.getCurrentNavigation().extras.preserveFragment !== undefined) {
+    /*if (router.getCurrentNavigation().extras.preserveFragment !== undefined) {
       this.message = this.router.getCurrentNavigation().extras.preserveFragment.valueOf();
-    }
+    }*/
   }
 
   ngOnInit(): void {
   }
+
+  public delete(): void{
+    this.message = false;
+}
 
 }
